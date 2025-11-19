0315650265ff6d9615b2dcae5e30516e1dda11c2
isaaceullen <146856994+isaaceullen@users.noreply.github.com>
2025-11-19 08:19:52 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Define o caminho base como relativo para funcionar no GitHub Pages
+})
